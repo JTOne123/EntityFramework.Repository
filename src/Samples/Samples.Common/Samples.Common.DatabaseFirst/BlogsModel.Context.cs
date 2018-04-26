5a5ddfbaa9772daef30a6d03d5581184eb3cd617
alex <thruthseekers@mail.ru>
2018-04-26 03:50:02 +0300

    recreated edmx

--- src/Samples/Samples.Common/Samples.Common.DatabaseFirst/BlogsModel.Context.cs
+++ src/Samples/Samples.Common/Samples.Common.DatabaseFirst/BlogsModel.Context.cs
@@ -13,10 +13,10 @@ namespace Samples.Common.DatabaseFirst
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class Entities : DbContext
+    public partial class BlogsContext : DbContext
     {
-        public Entities()
-            : base("name=Entities")
+        public BlogsContext()
+            : base("name=BlogsContext")
         {
         }
     
