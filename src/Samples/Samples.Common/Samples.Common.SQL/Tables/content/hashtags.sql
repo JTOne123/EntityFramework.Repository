9ee53e8ce2089d235d5cb52a6f2a027ca1fe2a5e
alex <thruthseekers@mail.ru>
2018-04-26 03:35:15 +0300

    sql server publish ready

--- src/Samples/Samples.Common/Samples.Common.SQL/Tables/content/hashtags.sql
+++ src/Samples/Samples.Common/Samples.Common.SQL/Tables/content/hashtags.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[hashtags]
 (
-	[Id] INT NOT NULL PRIMARY KEY,
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	Name NVARCHAR(64) NOT NULL UNIQUE,
+	-- DateCreate DateModified
 	--		AS Hitcount -- TODO: well...
 )
--- src/Samples/Samples.Common/Samples.Common.SQL/Tables/users/users.sql
+++ src/Samples/Samples.Common/Samples.Common.SQL/Tables/users/users.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[users]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Username] NVARCHAR(64) NOT NULL, 
 	-- just an example, no encryption
     [Password] NVARCHAR(64) NOT NULL, 
-    [DateRegistered] DATETIME NOT NULL DEFAULT GETDATE()
+    [DateRegistered] DATETIME NOT NULL DEFAULT GETUTCDATE()
 )
