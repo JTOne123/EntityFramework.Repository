d7b7a9e9a0247f9bbc5c8a4ef8f8dffb65a6c246
alex <thruthseekers@mail.ru>
2018-04-26 04:10:47 +0300

    to previous commit

---
+++ src/Samples/Samples.Basic/Samples.Basic.HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.Basic.HelloWorld
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+
+		}
+	}
+}
