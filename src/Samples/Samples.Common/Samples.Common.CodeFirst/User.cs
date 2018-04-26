8cd8fb9f4df4444dceabf27bf84b43ed43da83da
alex <thruthseekers@mail.ru>
2018-04-26 03:53:39 +0300

    added codefirst

---
+++ src/Samples/Samples.Common/Samples.Common.CodeFirst/User.cs
@@ -0,0 +1,38 @@
+namespace Samples.Common.CodeFirst
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class User
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            Bans = new HashSet<Ban>();
+            Posts = new HashSet<Post>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Password { get; set; }
+
+        public DateTime DateRegistered { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Ban> Bans { get; set; }
+
+        public virtual Moderator Moderator { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
